55cac005b0ce1012925208b6160f0678b5fb2c85
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2024-08-26 13:17:46 +1200

    make a simple color scheme

---
+++ beigegreen.nix
@@ -0,0 +1,13 @@
+# Attribute set with some colors so I can pass it into config files
+# I will use https://github.com/nutsalhan87/nix-colorizer for making
+# stuff lighter and darker
+{
+  # Matcha green
+  primary = "#A9A63F";
+  # Cyan
+  secondary = "#7BAFAD";
+  # Beige
+  neutral = "#FEFAA7";
+  # Brown
+  dark = "#482210";
+}
