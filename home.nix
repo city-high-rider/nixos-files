a90d684914384cb6f610af796a9762c21195df1d
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2024-08-26 23:05:31 +1200

    Interpolate color palette colors

--- home.nix
+++ home.nix
@@ -83,14 +83,14 @@ in {
       inherit (config.lib.formats.rasi) mkLiteral;
     in {
         "*" = {
-            bg0 = mkLiteral "#212121F2";
-            bg1 = mkLiteral "#2A2A2A";
-            bg2 = mkLiteral "#3D3D3D80";
-            bg3 = mkLiteral "#4CAF50F2";
-            fg0 = mkLiteral "#E6E6E6";
-            fg1 = mkLiteral "#FFFFFF";
-            fg2 = mkLiteral "#969696";
-            fg3 = mkLiteral "#3D3D3D";
+            bg0 = mkLiteral "#${csh.base01}F2";
+            bg1 = mkLiteral "#${csh.base02}";
+            bg2 = mkLiteral "#${csh.base03}80";
+            bg3 = mkLiteral "#${csh.primary}F2";
+            fg0 = mkLiteral "#${csh.base06}";
+            fg1 = mkLiteral "#${csh.base07}";
+            fg2 = mkLiteral "#${csh.base04}";
+            fg3 = mkLiteral "#${csh.base02}";
 
             font =   "Roboto 12";
 
