de4f18b539aeaf1fb4bff7aaf8e03d38aa33feb7
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2024-08-27 14:17:01 +1200

    add hypridle and hyprlock

--- home.nix
+++ home.nix
@@ -70,6 +70,45 @@ in {
     };
   };
 
+  # Hyprlock, screen locking utility.
+  programs.hyprlock = {
+    enable = true;
+    settings = {
+        general = {
+        disable_loading_bar = true;
+        grace = 300;
+        hide_cursor = true;
+        no_fade_in = false;
+      };
+
+      background = [
+        {
+          path = "screenshot";
+          blur_passes = 3;
+          blur_size = 8;
+        }
+      ];
+
+      input-field = [
+        {
+          size = "200, 50";
+          position = "0, -80";
+          monitor = "";
+          dots_center = true;
+          fade_on_empty = false;
+          font_color = "rgb(202, 211, 245)";
+          inner_color = "rgb(91, 96, 120)";
+          outer_color = "rgb(24, 25, 38)";
+          outline_thickness = 5;
+          placeholder_text =  ''
+          <span foreground="##cad3f5">Password...</span>
+          '';
+          shadow_passes = 2;
+        }
+      ]; 
+    };
+  };
+
   # Notification daemon.
   services.mako = {
     enable = true;
@@ -78,6 +117,39 @@ in {
     borderSize = 4;
     backgroundColor = "#${csh.base02}";
     textColor = "#${csh.base06}";
+  };
+
+  # No need to exec-once this, home-manager starts it automatically.
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        # Avoid launching multiple hyprlock instances.
+        lock_cmd = "pidof hyprlock || hyprlock";
+        # Lock when we go to sleep.
+        before_sleep_cmd = "loginctl lock-session";
+        # Avoid having to press a key twice to turn on display.
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+      listener = [
+        # After 2min, dim the screen.
+        { timeout = 150;
+          # Save old level, set to 10.
+          on-timeout = "brightnessctl -s set 10";
+          # Restore light level
+          on-resume = "brightnessctl -r";
+        }
+        # Lock screen after 5 mins
+        { timeout = 300;
+          on-timeout = "loginctl lock-session";
+        }
+        # 30s later, turn off the screen.
+        { timeout = 330;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms off";
+        }
+      ]; 
+    };
   };
 
 }
