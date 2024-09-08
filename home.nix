221e2db8beebe7948e89e8361ce67429bd373229
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2024-09-08 16:30:17 +1200

    Add OBS screen recorder

--- home.nix
+++ home.nix
@@ -152,4 +152,13 @@ in {
     };
   };
 
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
+  };
+
 }
