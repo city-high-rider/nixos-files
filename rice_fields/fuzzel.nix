312bf5f98d2a4b1a46d800c97a796a8b9556a6fe
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2025-01-19 16:30:29 +1300

    add colors and change font size of fuzzel

--- rice_fields/fuzzel.nix
+++ rice_fields/fuzzel.nix
@@ -1,1 +1,22 @@
-{ ... }: { programs.fuzzel.enable = true; }
+{ ... }:
+let csh = import ../beigegreen.nix;
+in {
+  programs.fuzzel.enable = true;
+  programs.fuzzel.settings = {
+    main = {
+      font = "JetBrains Mono NF:size=22";
+      use-bold = "yes";
+      dpi-aware = "no";
+      prompt = "I want a... ";
+    };
+    colors = {
+      background = "{csh.base01}f2";
+      prompt = "${csh.primary}f2";
+      selection-match = "${csh.secondary}f2";
+      match = "${csh.secondary}f2";
+      text = "${csh.base07}f2";
+      input = "${csh.base06}f2";
+      selection-text = "${csh.base07}f2";
+    };
+  };
+}
