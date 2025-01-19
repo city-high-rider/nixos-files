a4397d0b28d175ea00d103afbec410233ed0749e
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2025-01-19 15:19:45 +1300

    add niri flake to inputs and load it as a nixos module so we can use it in our config

--- flake.nix
+++ flake.nix
@@ -7,14 +7,18 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    niri.url = "github:sodiboo/niri-flake";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, niri, ... }:
     let system = "x86_64-linux";
     in {
       nixosConfigurations.omen15 = nixpkgs.lib.nixosSystem {
         inherit system;
-        modules = [ ./configuration.nix ];
+        # NOTE: The niri module will use a binary cache that is run by some guy,
+        # so I am trusting the guy in question to NOT upload malicious software there.
+        # docs: https://github.com/sodiboo/niri-flake/blob/main/docs.md
+        modules = [ ./configuration.nix niri.nixosModules.niri ];
       };
 
       homeConfigurations.nick = home-manager.lib.homeManagerConfiguration {
