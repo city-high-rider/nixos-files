59d987677f534dc2517e4df633deb33ddf4cc921
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2025-07-09 17:41:32 +1200

    make stub for new colorscheme

---
+++ colorschemes/redline.nix
@@ -0,0 +1,19 @@
+{
+  # Colors are in hex.
+  lightest = "";
+  lighter = "";
+  light = "";
+  neutral = "";
+  dark = "";
+  darker = "";
+  darkest = "";
+  #===========
+  red = "";
+  orange = "";
+  yellow = "";
+  green = "";
+  cyan = "";
+  blue = "";
+  brown = "";
+  purple = "";
+}
