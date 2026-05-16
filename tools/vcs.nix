e84b03d47538108a9fbac790554aaf952ab4be04
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2026-05-16 18:12:55 +1200

    these settings were renamed

--- tools/vcs.nix
+++ tools/vcs.nix
@@ -1,9 +1,9 @@
 { ... }: {
   programs.git = {
     enable = true;
-    userName = "city-high-rider";
-    userEmail = "60808209+city-high-rider@users.noreply.github.com";
-    extraConfig.credential = {
+    settings.user.name = "city-high-rider";
+    settings.user.email = "60808209+city-high-rider@users.noreply.github.com";
+    settings.credential = {
       "https://codeberg.org/" = { helper = "store"; };
       "https://github.com/" = { helper = "!gh auth git-credential"; };
       "https://gist.github.com/" = { helper = "!gh auth git-credential"; };
