6ab4c3a5be6657175fd18fa1f51e541b8212e212
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2026-05-16 18:07:59 +1200

    configure git credential store for codeberg

--- tools/vcs.nix
+++ tools/vcs.nix
@@ -4,6 +4,7 @@
     userName = "city-high-rider";
     userEmail = "60808209+city-high-rider@users.noreply.github.com";
     extraConfig.credential = {
+      "https://codeberg.org/" = { helper = "store"; };
       "https://github.com/" = { helper = "!gh auth git-credential"; };
       "https://gist.github.com/" = { helper = "!gh auth git-credential"; };
     };
