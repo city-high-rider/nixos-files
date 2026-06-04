20eaf7700e4bae37941b7f78b6301b6e96c36a45
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2026-06-04 12:34:41 +1200

    try to package focusclock

---
+++ focusclock.nix
@@ -0,0 +1,35 @@
+{
+  description = "A flake for focus clock";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      focusclock-pkg = pkgs.stdenv.mkDerivation {
+        pname = "focusclock";
+        version = "2.0.0";
+        src = pkgs.fetchFromGitHub {
+          owner = "KorigamiK";
+          repo = "focusclock";
+          rev = "main";
+          hash = "";
+        };
+        nativeBuildInputs = with pkgs; [
+          cmake
+          pkg-config
+          wrapGAppsHook4
+        ];
+        buildInputs = with pkgs; [
+          gtkmm4
+          gtk4-layer-shell
+        ];
+      };
+    in
+    {
+      packages.${system}.default = focusclock-pkg;
+      overlays.default = final: prev: {
+        focusclock = focusclock-pkg;
+      };
+    };
+}
